20ac28656f4e95bba945ed7e046ea3b029aa3246
rob <robertahuber88@gmail.com>
2019-08-03 19:36:02 +0200

    version 3 Aug

--- src/Components/MovieGallery.jsx
+++ src/Components/MovieGallery.jsx
@@ -1,30 +1,47 @@
 import React, { Component } from "react";
-import {
-  Card,
-  CardImg,
-  CardBody,
-  CardTitle,
-  CardSubtitle,
-} from "reactstrap";
+import { Row, Col } from "reactstrap";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
 
 class MovieGallery extends Component {
   render() {
-    return this.props.movies.map(item => (
-
-   
-  
-        <Card className ="card"    key={item.imdbID}>
-          <CardImg fluid top width="50%" height="auto" src={item.Poster} alt={item.Title}/>
-          <CardBody>
-            <CardTitle>{item.Title}</CardTitle>
-            <CardSubtitle>{item.Year}</CardSubtitle>
-            {/* <Button>Button</Button> */}
-          </CardBody>
-        </Card>
-
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1
     
-    ));
+    };
+    return (
+      <Slider slidesPerRow="6" {...settings}>
+        {this.props.movies.map(item => (
+          <div key={item.imdbID}>
+            <img width="50%" height="auto" src={item.Poster} alt={item.Title} />
+              <h5>{item.Title}</h5>
+          </div>
+        ))}
+      </Slider>
+    );
   }
 }
+
+// class MovieGallery extends Component {
+//   render() {
+//     return this.props.movies.map(item => (
+
+//         <Card className ="card"    key={item.imdbID}>
+//           <CardImg fluid top width="50%" height="auto" src={item.Poster} alt={item.Title}/>
+//           <CardBody>
+//             <CardTitle>{item.Title}</CardTitle>
+//             <CardSubtitle>{item.Year}</CardSubtitle>
+//             {/* <Button>Button</Button> */}
+//           </CardBody>
+//         </Card>
+
+//     ));
+//   }
+// }
 
 export default MovieGallery;
--- src/Components/SizzleVuMain.jsx
+++ src/Components/SizzleVuMain.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import NavBar from "./NavBar";
 import MovieGallery from "./MovieGallery";
-import {Container, CardColumns } from "reactstrap";
+import {Container, CardColumns, Row } from "reactstrap";
 
 class SizzleVuMain extends Component {
   constructor(props) {
@@ -27,17 +27,17 @@ class SizzleVuMain extends Component {
         <NavBar triggerSearch={this.search} />
         <Container fluid className="main">
           <h1>Harry Potter</h1>
-          <CardColumns>
+  
             <MovieGallery movies={this.state.harryPotter.filter(movie => movie.Title.includes(this.state.searchValue))} />
-          </CardColumns>
+         
           <h1>Lord of the Rings</h1>
-          <CardColumns>
+        
             <MovieGallery movies={this.state.lor.filter(movie => movie.Title.includes(this.state.searchValue))} />
-          </CardColumns>
+      
           <h1>Hobbit</h1>
-          <CardColumns>
+        
             <MovieGallery movies={this.state.hobbit.filter(movie => movie.Title.includes(this.state.searchValue))} />
-          </CardColumns>
+      
         </Container>
       </>
     );
