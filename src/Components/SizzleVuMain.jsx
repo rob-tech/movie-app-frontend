cf3623776f0dd95601ff43a5a5145fcef76c117a
rob <robertahuber88@gmail.com>
2019-09-22 23:30:41 +0200

    sundayCommit

--- src/Components/SizzleVuMain.jsx
+++ src/Components/SizzleVuMain.jsx
@@ -1,60 +1,42 @@
 import React, { Component } from "react";
-import NavBar from "./NavBar";
 import MovieGallery from "./MovieGallery";
-import { Container, Row} from "reactstrap";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { Row } from "reactstrap";
+import { BrowserRouter as Router } from "react-router-dom";
 // import { withRouter } from "react-router";
-import MovieDetails from "./MovieDetails";
-import AccountProfile from "./AccountProfile";
+
 
 class SizzleVuMain extends Component {
-  
   constructor(props) {
     super(props);
     this.state = {
       collections: [
-        { title: "Harry Potter", movies: [] },
-        { title: "Lord of the rings", movies: [] },
-        { title: "Hobbit", movies: [] }
-    ],
-       selectedMovie: null,
-       searchValue: "",
+        { Title: "HARRY POTTER", movies: [] },
+        { Title: "LORD OF THE RINGS", movies: [] },
+        { Title: "HOBBIT", movies: [] }
+      ],
+      selectedMovie: null
     };
   }
- 
-   search = (value) => {
-      this.setState({searchValue: value});
-      console.log(value)
-    }
 
-  
+  //movies={filteredItems} filteredMovie={this.state.genericMovie} filteredTitle={this.state.genericTitle} onMovieClicked={(imdbID) => this.setState({ selectedMovie: imdbID })}
+
   render() {
-    
     return (
-   <Router>
+      <Router>
         <>
-        <NavBar triggerSearch={this.search} />
-        <Route path="/" exact render={() => 
-         <Container fluid className="main">
-          
-          {this.state.collections.map((collectionsObject, index) => { 
-            var filteredItems = collectionsObject.movies.filter(movie => movie.Title.toLowerCase().includes(this.state.searchValue))
+          {this.state.collections.map((collectionsObject, index) => {
+            // var filteredItems = collectionsObject.movies.filter(movie => movie.Title.toLowerCase().includes(this.state.genericMovie))
             return (
-            <div key={index}>
-            {filteredItems.length > 0 &&
-              <Row className = "titleRow">{collectionsObject.title}</Row>
-            }
-              <MovieGallery movies={filteredItems} onMovieClicked={(imdbID) => this.setState({ selectedMovie: imdbID })} />
-            </div>
-          )})
-          } 
-     
-        </Container>
-        }/>
-            <Route path="/accountprofile" exact component={AccountProfile} />    
-             <Route path="/moviedetails/:imdbID" component={MovieDetails} />
-         </>
-         </Router>
+              <div key={index}>
+                  <Row className="titleRow">{collectionsObject.Title}</Row>
+                   <MovieGallery movies={collectionsObject.movies} onMovieClicked={imdbID => this.setState({ selectedMovie: imdbID })
+                    }
+                  />              
+              </div>
+            );
+          })}
+        </>
+      </Router>
     );
   }
 
@@ -65,49 +47,37 @@ class SizzleVuMain extends Component {
   };
 
   getMoviesHP = async () => {
-    var response = await fetch(
-      "http://www.omdbapi.com/?apikey=448f4427&s=harry%20potter&type=movie"
-    );
-    var moviesArray = await response.json();
-    var collection = this.state.collections;
-    collection[0].movies = moviesArray.Search;
-
+    var response = await fetch("http://localhost:3000/movies/harrypotter");
+    var hpmovies = await response.json();
+    var allMovies = this.state.collections;
+    allMovies[0].movies = hpmovies;
     this.setState({
-      collections: collection
+      collections: allMovies
     });
-
-    console.log(collection[0].movies);
   };
 
   getMoviesLOR = async () => {
-    var response = await fetch(
-      "http://www.omdbapi.com/?apikey=448f4427&s=lord%20of%20the%20rings&type=movie"
-    );
-    var moviesArray = await response.json();
-    var collection = this.state.collections;
-    collection[1].movies = moviesArray.Search;
-
+    var response = await fetch("http://localhost:3000/movies/lor");
+    var movies = await response.json();
+    var allMovies = this.state.collections;
+    allMovies[1].movies = movies;
     this.setState({
-      collections: collection
+      collections: allMovies
     });
-
-    console.log(collection[1].movies);
   };
 
   getMoviesHobbit = async () => {
-    var response = await fetch(
-      "http://www.omdbapi.com/?apikey=448f4427&s=Hobbit&type=movie"
-    );
-    var moviesArray = await response.json();
-    var collection = this.state.collections;
-    collection[2].movies = moviesArray.Search;
+    var response = await fetch("http://localhost:3000/movies/hobbit");
 
+    var movies = await response.json();
+    var allMovies = this.state.collections;
+    allMovies[2].movies = movies;
     this.setState({
-      collections: collection
+      collections: allMovies
     });
-
-    console.log(collection[2].movies);
   };
+
+
 }
 
 export default SizzleVuMain;
